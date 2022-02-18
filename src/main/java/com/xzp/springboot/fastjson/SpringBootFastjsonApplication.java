04e699c656970f714d6f90bb7f1951da1f5c926b
xuzhipeng <xuzhipeng@nucarf.com>
2022-02-18 17:34:31 +0800

    init

---
+++ src/main/java/com/xzp/springboot/fastjson/SpringBootFastjsonApplication.java
@@ -0,0 +1,18 @@
+package com.xzp.springboot.fastjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author xuzhipeng
+ * @date 2022/2/18
+ */
+@SpringBootApplication
+public class SpringBootFastjsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFastjsonApplication.class, args);
+    }
+
+}
